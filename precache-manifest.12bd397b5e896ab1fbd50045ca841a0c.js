67199ac957a95a1e6e8b13443f4774fba9ccbec8
xeyal_i <xeyal.isaxanov@gmail.com>
2019-07-26 02:18:53 +0400

    Updates

---
+++ precache-manifest.12bd397b5e896ab1fbd50045ca841a0c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d843ef1630b1d75a43dae4cd0a5df94",
+    "url": "/messageapp/index.html"
+  },
+  {
+    "revision": "a304bd422806d35a1dff",
+    "url": "/messageapp/static/css/main.8baedb96.chunk.css"
+  },
+  {
+    "revision": "666dd9c4bcfe986fac38",
+    "url": "/messageapp/static/js/2.b89041cf.chunk.js"
+  },
+  {
+    "revision": "a304bd422806d35a1dff",
+    "url": "/messageapp/static/js/main.49db5e64.chunk.js"
+  },
+  {
+    "revision": "5c9aa2cbbac496707282",
+    "url": "/messageapp/static/js/runtime~main.c946dfba.js"
+  }
+]);
